540499046f14d919e1b12b8ff54905d9acb4b5d0
Mathis Chekraoui <mathis.chekraoui@hotmail.com>
2025-06-10 10:39:19 +0200

    Corge

--- src/main/java/com.jad/Foo.java
+++ src/main/java/com.jad/Foo.java
@@ -1,13 +1,45 @@
 package com.jad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Foo {
     private final Bar bar;
+    private final ArrayList<Baz> bazs = new ArrayList<>();
+    private final Qux qux = new Qux();
+    private Corge corge;
 
     public Foo(final Bar bar) {
         this.bar = bar;
     }
 
+    public List<Baz> getBazs() {
+        return new ArrayList<>(this.bazs);
+    }
+
+    public void addBaz(final Baz baz) {
+        this.bazs.add(baz);
+    }
+
     public Bar getBar() {
         return this.bar;
+    }
+
+    public Qux getQux() {
+        return this.qux;
+    }
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(final Corge corge) {
+        if (this.corge != null) {
+            this.corge.setFoo(null);
+        }
+        this.corge = corge;
+        if (this.corge.getFoo() != this) {
+            this.corge.setFoo(this);
+        }
     }
 }
